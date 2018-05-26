15c58dcaa1dbd68677951b13d84a7b97c5fd4c6f
Do Thien Phu <tpo@elca.vn>
2018-05-26 15:24:46 +0700

    aaaaaaaaaaaa

--- partials/header.php
+++ partials/header.php
@@ -5,15 +5,30 @@
   
   <div class="site-header__main">
     <div class="container">
-      <div class="slds-grid slds-gutters slds-grid_vertical-align-center">
-        <div class="slds-col slds-size_3-of-12">
-          <?php include 'header_logo.php'; ?>
+      <div class="slds-grid slds-wrap slds-gutters slds-grid_vertical-align-center">
+        <div class="slds-col slds-size_12-of-12 slds-large-size_3-of-12">
+          <div class="site-header__main-left">
+            <?php include 'header_logo.php'; ?>
+            
+            <div id="main-nav-toggle" class="mmm-rw-top-menu-toggle mmm-toggle-wrapper">
+              <button type="button" class="mmm-toggle" aria-controls="" aria-pressed="false">
+                <i class="fa fa-lg fa-bars mmm-toggle__icon mmm-toggle__icon--open"></i>
+                <i class="fa fa-lg fa-times mmm-toggle__icon mmm-toggle__icon--close"></i>
+              </button>
+            </div>
+          </div>
         </div>
-        <div class="slds-col slds-size_7-of-12">
-          <?php include 'header_search.php'; ?>
-        </div>
-        <div class="slds-col slds-size_2-of-12">
-          <?php include 'header_cart.php'; ?>
+        <div class="slds-col slds-size_12-of-12 slds-large-size_9-of-12">
+          <div class="site-header__main-right">
+            <div class="slds-grid slds-gutters slds-grid_vertical-align-center">
+              <div class="slds-col slds-size_9-of-12">
+                <?php include 'header_search.php'; ?>
+              </div>
+              <div class="slds-col slds-size_3-of-12">
+                <?php include 'header_cart.php'; ?>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- partials/header_search.php
+++ partials/header_search.php
@@ -1,24 +1,16 @@
 <div class="header-search">
-  <button class="search-toggler navbar-toggler offcanvas-toggle btn" type="button" 
-    data-toggle="offcanvas"
-    data-target="#js-search-offcanvas">
-    <span class="search-toggler-icon"></span>
-    <span class="search-toggler-text">Tìm kiếm</span>
-  </button>
-  <div class="navbar-offcanvas navbar-offcanvas-touch" id="js-search-offcanvas">
-    <div class="form-search-wrapper">
-      <form class="form-search">
-        <div class="form-search__wrap typeahead__container">
-          <div class="form-search__input typeahead__field">
-            <span class="typeahead__query">
-              <input type="text" name="q" autocomplete="off" placeholder="Tìm sản phẩm, danh mục hay thương hiệu mong muốn ..." class="js-autocomplete-input form-control" />
-            </span>
-          </div>
-          <div class="form-search__actions typeahead__button">
-            <button type="submit" class="btn btn-search red"><i class="fa fa-search"></i> <span>Tìm kiếm</span></button>
-          </div>
+  <div class="form-search-wrapper">
+    <form class="form-search">
+      <div class="form-search__wrap typeahead__container">
+        <div class="form-search__input typeahead__field">
+          <span class="typeahead__query">
+            <input type="text" name="q" autocomplete="off" placeholder="Tìm sản phẩm, danh mục hay thương hiệu mong muốn ..." class="js-autocomplete-input form-control" />
+          </span>
         </div>
-      </form>
-    </div>
+        <div class="form-search__actions typeahead__button">
+          <button type="submit" class="btn btn-search red"><i class="fa fa-search"></i> <span>Tìm kiếm</span></button>
+        </div>
+      </div>
+    </form>
   </div>
 </div>
