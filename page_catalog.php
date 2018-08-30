caa4ccad2ab6a3c04502b05eea25761fe068555a
Do Thien Phu <tpo@elca.vn>
2018-08-30 18:00:34 +0700

    aaaaaaaa

--- page_catalog.php
+++ page_catalog.php
@@ -11,80 +11,154 @@
       <?php include 'partials/breadcrumb.php';?>
       
       <div class="container">
-        <div class="page-title-wrapper">
-          <h1 class="page-title">
-            <span class="base" data-ui-id="page-title-wrapper">Electronics</span>
-          </h1>
-        </div>
-        <?php $products = [1, 2, 3, 4, 5]; ?>
-        <div class="list-products list-products-grid">
-          <div class="slds-grid slds-wrap slds-gutters">
-            <?php foreach($products as $product) { ?>
-            <div class="list-products__item slds-col slds-size_12-of-12 slds-medium-size_6-of-12 slds-large-size_4-of-12 slds-x-large-size_3-of-12">
-              <div class="list-products__item-info">
-                <div class="list-products__item-inner">
-                  <div class="box-image">
-                    <a href="javascript:;" class="product-image-container">
-                      <span class="product-image-wrapper"> 
-                        <img class="product-image-photo"
-                        src="dist/img/product/<?php echo $product ?>.jpg"
-                        width="300" height="300" alt="Fuhlen Mouse for Gamer" />
-                      </span>
-                    </a>
+        <div class="slds-grid slds-wrap slds-gutters">
+          <div class="slds-col slds-size_12-of-12 slds-large-size_3-of-12 slds-order_2 slds-large-order_1">
+            <div class="sidebar sidebar--left">
+              <div class="frame frame-default frame-type-menu_subpages frame-layout-0 frame-background-none frame-has-backgroundimage frame-space-before-none frame-space-after-none">
+                <div class="frame-container">
+                  <div class="frame-inner">
+                    <header><h3 class="element-header ">
+                        Thương hiệu ắc quy ô tô
+                      </h3></header>
+                    <ul class="menu-subpages">
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-ac-quy/daewoo/" title="DAEWOO">DAEWOO</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-ac-quy/yuasa/" title="YUASA">YUASA</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-ac-quy/toplite/" title="TOPLITE">TOPLITE</a>
+                      </li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-ac-quy/gs/" title="GS">GS</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-ac-quy/amaron/" title="AMARON">AMARON</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-ac-quy/dong-nai/" title="Đồng Nai">Đồng
+                          Nai</a></li>
+                    </ul>
                   </div>
-
-                  <div class="list-products__item-details">
-                    <h2 class="product-name">
-                      <a href="javascript:;">Ắc quy DAEWOO 40B19FL 12V 35Ah</a>
-                    </h2>
-                    <div class="list-products__item-price">
-                      <span class="price-wrapper">
-                        <span class="price price--final">1.200.000 đ</span>
-                        <span class="price price--old">1.400.000 đ</span>
-                      </span>
-                    </div>
-
-                    <div class="list-products__item-actions">
-                      <form action="javascript:;" method="post">
-                        <input type="hidden" name="product" value="2281">
-                        <button type="submit" class="btn btn-action btn-cart btn-sm">
-                          <i class="fa fa-cart-plus"></i>
-                          <span>Thêm vào giỏ</span>
-                        </button>
-                      </form>
-                    </div>
+                </div>
+              </div>
+              <div id="c368"
+                   class="frame frame-default frame-type-menu_subpages frame-layout-0 frame-background-none frame-has-backgroundimage frame-space-before-none frame-space-after-none"
+                   style="">
+                <div class="frame-container">
+                  <div class="frame-inner">
+                    <header><h3 class="element-header ">
+                        Thương hiệu xe ô tô
+                      </h3></header>
+                    <ul class="menu-subpages">
+                      <li><a href="/danh-muc/ac-quy-o-to/honda/" title="HONDA">HONDA</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/cadillac/" title="CADILLAC">CADILLAC</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/acura/" title="ACURA">ACURA</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/toyota/" title="TOYOTA">TOYOTA</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/fiat/" title="FIAT">FIAT</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/ferrari/" title="FERRARI">FERRARI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/bentley/" title="BENTLEY">BENTLEY</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/porsche/" title="PORSCHE">PORSCHE</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/jaguar/" title="JAGUAR">JAGUAR</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/mini/" title="MINI">MINI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/lexus/" title="LEXUS">LEXUS</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/land-rover/" title="LAND ROVER">LAND ROVER</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/suzuki/" title="SUZUKI">SUZUKI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/bmw/" title="BMW">BMW</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/audi/" title="AUDI">AUDI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/mercedes/" title="MERCEDES">MERCEDES</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/ford/" title="FORD">FORD</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/misubishi/" title="MITSUBISHI">MITSUBISHI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/isuzu/" title="ISUZU">ISUZU</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/mazda/" title="MAZDA">MAZDA</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/chervolet/" title="CHERVOLET">CHERVOLET</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-xe/hyundai/" title="HYUNDAI">HYUNDAI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/kia/" title="KIA">KIA</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/nissan/" title="NISSAN">NISSAN</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-xe/daewoo/" title="DAEWOO">DAEWOO</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/subaru/" title="SUBARU">SUBARU</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/volvo/" title="VOLVO">VOLVO</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/infiniti/" title="INFINITI">INFINITI</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/peugeot/" title="PEUGEOT">PEUGEOT</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/renault/" title="RENAULT">RENAULT</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/volkswagen/" title="VOLKSWAGEN">VOLKSWAGEN</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/daihatsu/" title="DAIHATSU">DAIHATSU</a></li>
+                      <li><a href="/danh-muc/ac-quy-o-to/theo-thuong-hieu-xe/ssangyong/" title="SSANGYONG">SSANGYONG</a>
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
             </div>
-            <?php } ?>
+          </div>
+          <div class="slds-col slds-size_12-of-12 slds-large-size_9-of-12 slds-order_1 slds-large-order_2">
+            <div class="page-title-wrapper">
+              <h1 class="page-title">
+                <span class="base" data-ui-id="page-title-wrapper">Electronics</span>
+              </h1>
+            </div>
+            <?php $products = [1, 2, 3, 4, 5]; ?>
+            <div class="list-products list-products-grid">
+              <div class="slds-grid slds-wrap slds-gutters">
+                <?php foreach($products as $product) { ?>
+                <div class="list-products__item slds-col slds-size_12-of-12 slds-medium-size_6-of-12 slds-large-size_4-of-12 slds-x-large-size_4-of-12">
+                  <div class="list-products__item-info">
+                    <div class="list-products__item-inner">
+                      <div class="box-image">
+                        <a href="javascript:;" class="product-image-container">
+                          <span class="product-image-wrapper">
+                            <img class="product-image-photo"
+                            src="dist/img/product/<?php echo $product ?>.jpg"
+                            width="300" height="300" alt="Fuhlen Mouse for Gamer" />
+                          </span>
+                        </a>
+                      </div>
+
+                      <div class="list-products__item-details">
+                        <h2 class="product-name">
+                          <a href="javascript:;">Ắc quy DAEWOO 40B19FL 12V 35Ah</a>
+                        </h2>
+                        <div class="list-products__item-price">
+                          <span class="price-wrapper">
+                            <span class="price price--final">1.200.000 đ</span>
+                            <span class="price price--old">1.400.000 đ</span>
+                          </span>
+                        </div>
+
+                        <div class="list-products__item-actions">
+                          <form action="javascript:;" method="post">
+                            <input type="hidden" name="product" value="2281">
+                            <button type="submit" class="btn btn-action btn-cart btn-sm">
+                              <i class="fa fa-cart-plus"></i>
+                              <span>Thêm vào giỏ</span>
+                            </button>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <?php } ?>
+              </div>
+            </div>
+
+            <ul class="pagination justify-content-end">
+              <li class="page-item">
+                <a class="page-link prev" href="javascript:;">
+                  <i class="fa fa-angle-left" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="page-item active">
+                <span class="page-link">1</span>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="javascript:;">2</a>
+              </li>
+              <li class="page-item disabled">
+                <a class="page-link" href="javascript:;">3</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="javascript:;">4</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link next" href="javascript:;">
+                  <i class="fa fa-angle-right" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-        
-        <ul class="pagination justify-content-end">
-          <li class="page-item">
-            <a class="page-link prev" href="javascript:;">
-              <i class="fa fa-angle-left" aria-hidden="true"></i>
-            </a>
-          </li>  
-          <li class="page-item active">
-            <span class="page-link">1</span>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="javascript:;">2</a>
-          </li>
-          <li class="page-item disabled">
-            <a class="page-link" href="javascript:;">3</a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="javascript:;">4</a>
-          </li>
-          <li class="page-item">
-            <a class="page-link next" href="javascript:;">
-              <i class="fa fa-angle-right" aria-hidden="true"></i>
-            </a>
-          </li>
-        </ul>
       </div>
     </main>
   </div>
---
+++ page_ototruyenthong.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<?php include_once 'partials/head.php'; ?>
+<body>
+
+<div id="page" class="site layout-bg-white">
+  <?php include_once 'partials/header.php'; ?>
+  
+  <div id="content" class="site-content">
+    <main id="main" class="site-main">
+      <?php include 'partials/breadcrumb.php';?>
+
+      <div class="frame">
+        <div class="container">
+          <h1 class="element-header ">
+            Lorem isum dolor sit amet
+          </h1>
+          <p>
+            Each fully backed by our worry-free, nationwide warranty, our car and truck batteries deliver the performance you deserve and the quality we've delivered since 1952. After all, there’s a reason Interstate batteries are the first choice for auto technicians. When you need to be somewhere, you need a car or truck battery with dependability you can trust; you need an Interstate battery.
+          </p>
+          <ul class="list-checked">
+            <li>Battery is one of the most important truck components responsible for engine start-up and reliable operation of all electrical systems</li>
+            <li>Commercial and special vehicles typically used in severe operating conditions that impose special requirements for batteries</li>
+            <li>Bosch Series T batteries – are a line of specific storage batteries for commercial and special vehicles that feature the highest reliability and performance</li>
+          </ul>
+          <p>
+            The first lead-acid storage battery, the predecessor of the modern starter battery, was introduced in 1859 by Gaston Plante. During experiments he found out that a stack of lead plates placed in electrolyte is much more effective than the other electrode materials. With the further advance of technology lead-acid batteries got widespread use and are still the fundamental battery type for road transport.
+          </p>
+        </div>
+      </div>
+      
+      <div class="frame">
+        <div class="container">
+          <div class="slds-grid slds-wrap slds-gutters">
+            <div class="slds-col slds-size_12-of-12 slds-large-size_6-of-12">
+              <div class="w-services">
+                <div class="row">
+                  <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="w-services__box">
+                      <div class="inner-box">
+                        <div class="icon-box">
+                          <i class="fa fa-gear"></i>
+                        </div>
+                        <h3>Define</h3>
+                        <div class="block-text">Podcasting operational change management inside of workflowsto establish the main a framework. </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 col-sm-6 col-xs-12 now-in-view">
+                    <div class="w-services__box">
+                      <div class="inner-box">
+                        <div class="icon-box">
+                          <i class="fa fa-gear"></i>
+                        </div>
+                        <h3>Design</h3>
+                        <div class="block-text">Podcasting operational change management inside of workflowsto establish the main a framework. </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="w-services__box">
+                      <div class="inner-box">
+                        <div class="icon-box">
+                          <i class="fa fa-gear"></i>
+                        </div>
+                        <h3>Develop</h3>
+                        <div class="block-text">Podcasting operational change management inside of workflowsto establish the main a framework. </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="w-services__box">
+                      <div class="inner-box">
+                        <div class="icon-box">
+                          <i class="fa fa-gear"></i>
+                        </div>
+                        <h3>Branding</h3>
+                        <div class="block-text">Podcasting operational change management inside of workflowsto establish the main a framework. </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="slds-col slds-size_12-of-12 slds-large-size_6-of-12">
+              <div class="gallery-row row">
+                <div class="gallery-item col-12">
+                  <figure class="image"><img src="/fileadmin/user_upload/14660452977561.png"
+                                             data-src="/fileadmin/user_upload/14660452977561.png"
+                                             data-large="/fileadmin/user_upload/14660452977561.png"
+                                             data-medium="/fileadmin/user_upload/14660452977561.png"
+                                             data-small="/fileadmin/user_upload/14660452977561.png"
+                                             data-extrasmall="/fileadmin/_processed_/8/2/csm_14660452977561_64b8c3e9aa.png"
+                                             title="" alt="" class="lazyload" data-preload="false" style="opacity: 1;">
+                    <noscript><img src="/fileadmin/user_upload/14660452977561.png" title="" alt=""/></noscript>
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="frame">
+        <div class="container">
+
+        </div>
+      </div>
+
+    </main>
+  </div>
+</div>
+<?php include 'partials/footer.php';?>
+</body>
+</html>
